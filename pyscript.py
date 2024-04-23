e2b69bfc06ab706176a0971564d696e3ed64df36
anthuny <118776713+anthunyy@users.noreply.github.com>
2024-04-23 09:47:39 -0700

    new screenshot

--- pyscript.py
+++ pyscript.py
@@ -1,11 +1,18 @@
 from PIL import ImageGrab
 import time
 import keyboard
+import os
 time.sleep(5)
 while True:
     screenshot = ImageGrab.grab()
     screenshot.save("screenshot.png")
     screenshot.close()
     screenshot = ImageGrab.grab(bbox=(100, 100, 500, 500))
+    os.system("cd mccodesite/mccodesite")
+    os.system("git add --all")
+    time.sleep(2.5)
+    os.system("git commit -m \"new screenshot\"")
+    time.sleep(2.5)
+    os.system("git push")
     time.sleep(3600)
 
